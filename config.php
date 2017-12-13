79b1c345e301d029f4cc76b6a296aa21c9927955
chapeau <mou.idri@gmail.com>
2017-12-13 13:14:25 +0100

    Add files via upload

--- config.php
+++ config.php
@@ -2,5 +2,5 @@
    $user = 'root'; 
    $password = 'password'; 
    $database = 'customersDB'; 
-   $db = new mysqli('10.100.3.4',$user,$password,$database); 
+   $db = new mysqli('ub-16-demo-db-mourad',$user,$password,$database); 
  ?>
--- sh_bootstrap_pri.sh
+++ sh_bootstrap_pri.sh
@@ -2,16 +2,22 @@
 apt-get update -y
 
 # install PHP and MySQL
-apt-get install git apache2 ifstat inetutils-traceroute traceroute telnet curl python python-pip python3 python3-pip libapache2-mod-python -y
-pip install requests ConfigParser mysql-connector-python flask-restful
+apt-get install git apache2 ifstat inetutils-traceroute traceroute telnet curl python python-pip python3 python3-pip libapache2-mod-python libapache2-mod-php php-mysql -y
+apt-get install mysql-client -y
+pip install requests flask ConfigParser mysql-connector-python flask-restful ast 
 pip3 install requests
-apt-get install php php-fpm php-mysql php-curl php-gd php-pear php-imagick php-imap php-mcrypt php-recode php-tidy php-xmlrpc -y
+apt-get install php php-fpm php-mysql php-curl php-gd php-pear php-imagick php-imap php-mcrypt php-recode php-tidy php-xmlrpc php-curl php-gd php-intl php-json php-mbstring php-mcrypt php-xml php-zip -y
 
-sudo a2enmod cgi
-#systemctl enable mariadb.service
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 52E16F86FEE04B979B07E28DB02C46DF417A0893
+apt-key adv --keyserver packages.microsoft.com --recv-keys 52E16F86FEE04B979B07E28DB02C46DF417A0893
+apt-get install apt-transport-https
+apt-get update && apt-get install azure-cli
 
-export DB_NAME=root
-export DB_USER=wordpress
+a2enmod cgi
+
+
+export DB_NAME=customersDB
+export DB_USER=root
 export DB_PWD=password
 
 
